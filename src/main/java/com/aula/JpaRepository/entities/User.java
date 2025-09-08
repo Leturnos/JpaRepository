c1a4dcefaef384274709cdad727d45993794fb94
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2025-09-08 15:30:37 -0300

    creation maven project and class user

---
+++ src/main/java/com/aula/JpaRepository/entities/User.java
@@ -0,0 +1,51 @@
+package com.aula.JpaRepository.entities;
+
+public class User {
+    private long id;
+    private String name;
+    private String email;
+    private double salary;
+
+    public User () {
+
+    }
+
+    public User(long id, String name, String email, double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
